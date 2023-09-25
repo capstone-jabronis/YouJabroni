160f2b634c776ecd04ced5742da313ca3f47ab7e
Jose De La Luz <Jdelaluz01@gmail.com>
2023-09-25 16:20:40 -0500

    initial commit

---
+++ src/main/java/com/youjabroni/youjabronicapstone/YouJabroniCapstoneApplication.java
@@ -0,0 +1,13 @@
+package com.youjabroni.youjabronicapstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouJabroniCapstoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YouJabroniCapstoneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/youjabroni/youjabronicapstone/YouJabroniCapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youjabroni.youjabronicapstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouJabroniCapstoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
