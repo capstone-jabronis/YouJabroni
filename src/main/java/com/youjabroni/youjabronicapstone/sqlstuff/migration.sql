9d5a133de0aa6215a2ec39568527d63a5b6dff6e
Jose De La Luz <Jdelaluz01@gmail.com>
2023-09-26 11:04:05 -0500

    migration script

--- src/main/java/com/youjabroni/youjabronicapstone/sqlstuff/migration.sql
+++ src/main/java/com/youjabroni/youjabronicapstone/sqlstuff/migration.sql
@@ -1,3 +1,4 @@
+drop database jabroni_db;
 create DATABASE if not exists jabroni_db;
 use jabroni_db;
 drop table if exists users;
@@ -11,7 +12,7 @@ create table users (
 drop table if exists tournament;
 create table tournament (
                        id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-                       winner int,
+                       winner int unsigned,
                        start_time DATETIME not null,
                        FOREIGN KEY(winner) REFERENCES users(id)
 );
@@ -20,7 +21,7 @@ drop table if exists rounds;
 create table rounds (
                        id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
                        round_num smallint NOT NULL ,
-                       tournament_id int,
+                       tournament_id int unsigned,
                        meme_pic varchar(255) not null,
                        foreign key (tournament_id) references tournament(id)
 );
@@ -29,16 +30,16 @@ drop table if exists meme_submission;
 create table meme_submission (
                        id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
                        caption varchar(255) ,
-                       user_id int,
-                       round_id int,
+                       user_id int unsigned,
+                       round_id int unsigned,
                        foreign key(user_id) references users(id),
                        foreign key(round_id) references rounds(id)
 );
 
 drop table if exists votes;
 create table votes (
-                                 user_id int,
-                                 meme_submission_id int,
+                                 user_id int unsigned,
+                                 meme_submission_id int unsigned,
                                  foreign key(user_id) references users(id),
                                  foreign key(meme_submission_id) references meme_submission(id)
 );
