6f2864f7dd28beca5519606cc8f3129cd87fda70
Audrey Tan <audrey.wanmun.tan@gmail.com>
2023-09-27 10:26:05 -0500

    testing hover effects on nav and footer links. added partials to all html except tournament pages

--- src/main/java/com/youjabroni/youjabronicapstone/models/User.java
+++ src/main/java/com/youjabroni/youjabronicapstone/models/User.java
@@ -1,10 +1,10 @@
-package com.youjabroni.youjabronicapstone.models;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-
-@Entity
-@Table(name = "users");
-public class User {
-
-}
+//package com.youjabroni.youjabronicapstone.models;
+//
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.Table;
+//
+//@Entity
+//@Table(name = "users");
+//public class User {
+//
+//}
