42737b1c72b09753729325271733855959635bea
Jose De La Luz <Jdelaluz01@gmail.com>
2023-09-28 16:19:52 -0500

    fixed some imports

--- src/main/java/com/youjabroni/youjabronicapstone/controllers/PagesController.java
+++ src/main/java/com/youjabroni/youjabronicapstone/controllers/PagesController.java
@@ -4,11 +4,13 @@ package com.youjabroni.youjabronicapstone.controllers;
 import com.youjabroni.youjabronicapstone.models.MemeSubmission;
 import com.youjabroni.youjabronicapstone.models.User;
 import com.youjabroni.youjabronicapstone.repositories.TournamentRepository;
+import com.youjabroni.youjabronicapstone.repositories.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -16,6 +18,7 @@ import java.util.List;
 public class PagesController {
 
     private TournamentRepository tournamentDao;
+    private UserRepository userDao;
 
     public PagesController(TournamentRepository tournamentDao) {
         this.tournamentDao = tournamentDao;
