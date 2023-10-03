7b3bfd6250a729c781a548831c14dabe941e4cd7
Audrey Tan <audrey.wanmun.tan@gmail.com>
2023-10-03 13:52:23 -0500

    added some code to javascript and waiting-room html to connect websocket to frontend

--- src/main/resources/static/js/tournament.js
+++ src/main/resources/static/js/tournament.js
@@ -1,4 +1,35 @@
-(() => {
-
-
+(function() {
+    console.log("in tournament.js");
+    const tournamentId = document.querySelector('#tournamentID').text();
+    const csrfToken = document.querySelector('meta[name="_csrf"]').getAttribute('content');
+    const Tournament = {
+        stompClient: null,
+        tournamentId: tournamentId,
+        topic: null,
+        currentSubscription: null,
+    }
+    const Socket = {
+        connect() {
+            let socket = new SockJS("/secured/sock");
+            Tournament.stompClient = Stomp.over(socket);
+            Tournament.stompClient.connect({'X-CSRF-TOKEN': csrfToken}, this.onConnected, this.onError);
+        },
+        onConnected() {
+            console.log("inside onConnected");
+            Socket.enterRoom(Tournament.tournamentId);
+        },
+        onError(error) {
+            console.log("Error connecting to stream. Error:");
+            console.log(error);
+        },
+        enterRoom(tournamentId) {
+            console.log("Inside enterRoom");
+            Tournament.topic = `/secured/tournament/waiting-room/${Tournament.tournamentId}`;
+            Tournament.currentSubscription = Tournament.stompClient.subscribe(`/secured/tournament/${Tournament.tournamentId}`, this.onMessageReceived);
+            Tournament.stompClient.send(`${Tournament.topic}`, {}, "Hewwo");
+        },
+        onMessageReceived(payload) {
+            console.log("Inside onMessageReceived!");
+        }
+    }
 })();
