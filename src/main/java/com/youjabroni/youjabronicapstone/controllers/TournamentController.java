a523becf6f2129613434efa2b49b72c81e54a2a2
Jose De La Luz <Jdelaluz01@gmail.com>
2023-10-04 09:32:32 -0500

    snake game for waiting room

--- src/main/java/com/youjabroni/youjabronicapstone/controllers/TournamentController.java
+++ src/main/java/com/youjabroni/youjabronicapstone/controllers/TournamentController.java
@@ -50,6 +50,7 @@ public class TournamentController {
         return "tournament/complete";
     }
 
+
     @PostMapping("/waiting-room/{id}")
     public String joinTournament(@AuthenticationPrincipal UserDetails userDetails, Model model, @PathVariable Long id) {
         User user = userDao.findByUsername(userDetails.getUsername());
