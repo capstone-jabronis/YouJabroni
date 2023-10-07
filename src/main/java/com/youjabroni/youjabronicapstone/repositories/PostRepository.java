24be922194f2f711e3f5d1b725946ee8ad2e7202
Ava Smith <ava013005@gmail.com>
2023-10-06 21:44:33 -0500

    added a count for posts on the users profile

--- src/main/java/com/youjabroni/youjabronicapstone/repositories/PostRepository.java
+++ src/main/java/com/youjabroni/youjabronicapstone/repositories/PostRepository.java
@@ -4,6 +4,8 @@ import com.youjabroni.youjabronicapstone.models.Post;
 import com.youjabroni.youjabronicapstone.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
-    Post findByUserId(long id);
+    List<Post> findByUserId(long id);
 }
