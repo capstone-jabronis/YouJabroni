94fc0d176a2aacd522916b9289cc921a7a7de549
Nicholas Rowley <nicholas.ryan.rowley@gmail.com>
2023-10-10 15:11:46 -0500

    refactored waiting-room into lobby, render for waiting users works for joining and leaving, host is displayed in lobby, creating method for getting current host from tournament

--- src/main/java/com/youjabroni/youjabronicapstone/controllers/TournamentController.java
+++ src/main/java/com/youjabroni/youjabronicapstone/controllers/TournamentController.java
@@ -1,7 +1,6 @@
 package com.youjabroni.youjabronicapstone.controllers;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.youjabroni.youjabronicapstone.models.MemeSubmission;
 import com.youjabroni.youjabronicapstone.models.Tournament;
 import com.youjabroni.youjabronicapstone.models.User;
 import com.youjabroni.youjabronicapstone.models.Message;
@@ -13,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -51,12 +49,12 @@ public class TournamentController {
     }
 
     //Websocket stuff////////////////////////////
-    @MessageMapping("/tournament/waiting-room/{tournamentId}/userjoin")
+    @MessageMapping("/tournament/lobby/{tournamentId}/userjoin")
     public void joinMessage(@DestinationVariable Long tournamentId, @Payload Message joinMessage) throws JsonProcessingException {
         System.out.println("----------In sendMessage method for userjoin---------");
         joinMessage.setMessageType(Message.MessageType.JOIN);
         System.out.println(joinMessage);
-        messagingTemplate.convertAndSend(format("/secured/tournament/waiting-room/%s", tournamentId), joinMessage);
+        messagingTemplate.convertAndSend(format("/secured/tournament/lobby/%s", tournamentId), joinMessage);
 
 //        , SimpMessageHeaderAccessor headerAccessor
         //^For constructor
@@ -70,6 +68,13 @@ public class TournamentController {
 //        }
 ////        headerAccessor.getSessionAttributes().put("name", memeSubmission.getUser());
 //        messagingTemplate.convertAndSend(format("/secured/tournament/waiting-room/%s", tournamentId), joinMessage);
+    }
+    @MessageMapping("/tournament/lobby/{tournamentId}/send")
+    public void sendMessage(@DestinationVariable Long tournamentId, @Payload Message message) throws JsonProcessingException {
+        System.out.println("----------In sendMessage method---------");
+        System.out.println(message.getMessageType());
+//        message.setMessageType(Message.MessageType.JOIN);
+        messagingTemplate.convertAndSend(format("/secured/tournament/lobby/%s", tournamentId), message);
     }
 //End websocket stuff/////////////////////////////////
 
@@ -85,7 +90,7 @@ public class TournamentController {
     }
 
 
-    @GetMapping("/waiting-room/{id}")
+    @GetMapping("/lobby/{id}")
     public String joinTournament(@AuthenticationPrincipal UserDetails userDetails, Model model, @PathVariable Long id) {
         User user = userDao.findByUsername(userDetails.getUsername());
         Tournament tournament = tournamentDao.findById(id).get();
@@ -97,10 +102,11 @@ public class TournamentController {
         tournamentDao.save(tournament);
         model.addAttribute("tournament", tournament);
         model.addAttribute("users", tournament.getUserSet());
-        return "tournament/waiting-room";
+        model.addAttribute("currentUser", user);
+        return "/tournament/lobby";
     }
 
-    @GetMapping("/waiting-room/leave")
+    @GetMapping("/lobby/leave")
     public String leaveTournamentWaitingRoom(@AuthenticationPrincipal UserDetails userDetails) {
         User user = userDao.findByUsername(userDetails.getUsername());
         Tournament tournament = tournamentDao.findById(user.getTournament().getId()).get();
@@ -111,23 +117,16 @@ public class TournamentController {
         tournamentDao.save(tournament);
         System.out.println("LEFT TOURNY" + tournamentDao.findById(tournament.getId()).get().getUserSet());
         //Logic to change host or delete tournament if users set is empty
-        if(updateUserSet.isEmpty()){
+        if (updateUserSet.isEmpty()) {
             System.out.println("----------Tournament empty, deleting");
             tournamentDao.delete(tournament);
         } else {
             System.out.println("changing host to next user");
-            User newHost = tournament.getUserSet().iterator().next();
+            User newHost = updateUserSet.iterator().next();
             tournament.setHost(newHost);
             tournamentDao.save(tournament);
         }
         return "redirect:/home";
-    }
-
-    //Mapping to return and update users in tournament primarily for websocket
-    @GetMapping("/{tournamentId}/members")
-    public @ResponseBody Set<User> getTournamentMembers(@PathVariable Long tournamentId) {
-        Tournament tournament = tournamentDao.findById(tournamentId).get();
-        return tournament.getUserSet();
     }
 
     //Creating new tournaments
@@ -140,7 +139,21 @@ public class TournamentController {
         tournamentDao.save(newTournament);
         String id = String.valueOf(newTournament.getId());
         System.out.println("Tournament id: " + id);
-        return "redirect:/tournament/waiting-room/" + id;
+        return "redirect:/tournament/lobby/" + id;
+    }
+
+    //Mapping to return and update users in tournament primarily for websocket
+    @GetMapping("/{tournamentId}/members")
+    public @ResponseBody Set<User> getTournamentMembers(@PathVariable Long tournamentId) {
+        Tournament tournament = tournamentDao.findById(tournamentId).get();
+        return tournament.getUserSet();
+    }
+
+    //Mapping to return the current tournament host
+    @GetMapping("/{tournamentId}/host")
+    public @ResponseBody User getTournamentHost(@PathVariable Long tournamentId) {
+        Tournament tournament = tournamentDao.findById(tournamentId).get();
+        return tournament.getHost();
     }
 
 }
