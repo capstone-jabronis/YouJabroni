53aeaf0d77816745db9a6848f397141eecaf66a9
Nicholas Rowley <nicholas.ryan.rowley@gmail.com>
2023-10-11 12:10:25 -0500

    integrated meme api into lobby, rendered page, creating message mapping for meme submission, create event listners to grab meme info to send to server

--- src/main/resources/static/js/memeAPI.js
+++ src/main/resources/static/js/memeAPI.js
@@ -1,22 +1,22 @@
-const csrfToken = document.querySelector('meta[name="_csrf"]').getAttribute('content');
-let URL = "https://api.imgflip.com/get_memes";
-let imageContainer = document.querySelector(".memeAPIImage");
-
-(async () =>{
-    try {
-        const response = await fetch(URL);
-        const data = await response.json();
-        console.log(data)
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-
-        const memes = data.data.memes;
-        const randomMeme = memes[Math.floor(Math.random() * memes.length)];
-        const imageUrl = randomMeme.url;
-
-        imageContainer.src= imageUrl;
-    } catch (error) {
-        console.error('Error fetching meme:', error);
-    }
-})();
+// const csrfToken = document.querySelector('meta[name="_csrf"]').getAttribute('content');
+// let URL = "https://api.imgflip.com/get_memes";
+// let imageContainer = document.querySelector(".memeAPIImage");
+//
+// (async () =>{
+//     try {
+//         const response = await fetch(URL);
+//         const data = await response.json();
+//         console.log(data)
+//         if (!response.ok) {
+//             throw new Error(`HTTP error! Status: ${response.status}`);
+//         }
+//
+//         const memes = data.data.memes;
+//         const randomMeme = memes[Math.floor(Math.random() * memes.length)];
+//         const imageUrl = randomMeme.url;
+//
+//         imageContainer.src= imageUrl;
+//     } catch (error) {
+//         console.error('Error fetching meme:', error);
+//     }
+// })();
